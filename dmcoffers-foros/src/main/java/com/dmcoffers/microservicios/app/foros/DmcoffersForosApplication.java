e8bed370df7b302db1079e570742a0c3ede2065b
Ramiro Ceballos <rceballos.work@gmail.com>
2024-04-01 19:38:31 -0300

    inicializacion foros

---
+++ dmcoffers-foros/src/main/java/com/dmcoffers/microservicios/app/foros/DmcoffersForosApplication.java
@@ -0,0 +1,13 @@
+package com.dmcoffers.microservicios.app.foros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmcoffersForosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmcoffersForosApplication.class, args);
+	}
+
+}
---
+++ dmcoffers-foros/src/test/java/com/dmcoffers/microservicios/app/foros/DmcoffersForosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmcoffers.microservicios.app.foros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmcoffersForosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
