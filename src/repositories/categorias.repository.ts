7342c5c34f5beac772259a03be5cbc5fe0333144
Alex <alexnicolashoyos@gmail.com>
2024-04-15 19:41:14 -0300

    Modificado

--- src/repositories/categorias.repository.ts
+++ src/repositories/categorias.repository.ts
@@ -12,37 +12,3 @@ const categorias = [
     ),
 ]
 
-export class CategoriasRepository implements Repository<Categorias> {
-
-    public findAll(): Categorias[] | undefined {
-        return categorias
-    }
-
-    public findOne(categorias: { id: number }): Categorias | undefined {
-        return categorias.find((foroEnt) => categorias.id === foroEnt.id)
-    }
-
-    public add(categorias: Categorias): Categorias | undefined {
-        categorias.push(categorias)
-        return categorias
-    }
-
-    public update(categorias: Categorias): Categorias | undefined {
-        const categoriaIdx = categorias.findIndex((foroEnt) => categorias.id === foroEnt.id)
-
-        if (categoriaIdx > -1) {
-            categoriaIdx[categoriaIdx] = { ...categorias[categoriaIdx], ...categorias }
-        }
-        return categorias[categoriaIdx]
-    }
-
-    public delete(categorias: { id: number }): Categorias | undefined {
-        const categoriaIdx = categorias.findIndex((categoriaEnt) => categorias.id === categoriaEnt.id)
-
-        if (categoriaIdx > -1) {
-            const deletedCategoria = categorias[categoriaIdx]
-            categorias.splice(categoriaIdx, 1)
-            return deletedCategoria
-        }
-    }
-}
