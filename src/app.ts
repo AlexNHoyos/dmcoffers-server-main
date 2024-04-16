69df4a69ba28c37c335285685c8ec505708e5eb6
Mauro Marcosano <mauromarcosano@gmail.com>
2024-04-15 21:20:09 -0300

    config basica de Publisher CRUD (sin persistencia)

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express from 'express';
-import { characterRouter } from './character/character.routes.js';
+import { publisherRouter } from './routes/publisher.routes.js';
 
 const app = express();
 app.use(express.json());
 
-app.use('/api/characters', characterRouter);
+app.use('/api/publishers', publisherRouter);
 
 app.use((_, res) => {
   return res.status(404).send({ message: 'Resource not found' });
