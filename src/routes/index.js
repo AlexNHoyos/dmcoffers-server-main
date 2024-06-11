ae9b086c2e4475fc36c964094bc36cacffbd4172
Mauro Marcosano <mauromarcosano@gmail.com>
2024-06-11 11:22:57 -0300

    Add Bdd implement

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const personsRouter = require('./persons.router.js');
+
+function routerApi(app) {
+  const router = express.Router();
+  app.use('/api/v1', router);
+  router.use('/persons', personsRouter);
+}
+
+module.exports = routerApi;
