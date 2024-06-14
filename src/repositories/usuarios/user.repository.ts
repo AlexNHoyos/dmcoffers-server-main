aa11dd88aa45f8e1af470a55819f3d5b02f1c26b
Ignacio TR <ignacio_nob_93@hotmail.com>
2024-06-14 13:13:31 -0300

    modificados tambien para hacerlos transaccionales metodos UPDATE y DELETE del user.repository.ts

--- src/repositories/usuarios/user.repository.ts
+++ src/repositories/usuarios/user.repository.ts
@@ -62,26 +62,56 @@ export class UserRepository implements Repository<User> {
     }
 
     async update(id: string, user: User) {
-        const { realname, surname, username, birth_date, delete_date, status } = user;
-        const query = 'UPDATE swe_usrapl su SET realname = $1, surname = $2, username = $3, birth_date = $4, delete_date = $5, creationuser = current_user, creationtimestamp = current_timestamp, modificationuser = current_user, modificationtimestamp = current_timestamp, status = status RETURNING *;';
-        const values = [realname, surname, username, birth_date, delete_date, status]
-
-        try {
-            const result = await pool.query(query, values)
-            return result.rows[0];
-        } catch (error) {
-            console.error("Error al actualizar usuario", error)
-            throw error;
+            const { realname, surname, username, birth_date, delete_date, status } = user;
+            //arma la query de actualizcion
+            const query = `
+                UPDATE swe_usrapl su
+                SET
+                    realname = $1,
+                    surname = $2,
+                    username = $3,
+                    birth_date = $4,
+                    delete_date = $5,
+                    modificationuser = current_user,
+                    modificationtimestamp = current_timestamp,
+                    status = $6
+                WHERE su.id = $7
+                RETURNING *;
+            `;
+            const values = [realname, surname, username, birth_date, delete_date, status, id];
+    
+            const client = await pool.connect();
+    
+            try {
+                await client.query('BEGIN');
+                const result = await client.query(query, values);
+                await client.query('COMMIT');
+                return result.rows[0];
+            } catch (error) {
+                await client.query('ROLLBACK');
+                console.error("Error al actualizar el usuario", error);
+                throw error;
+            } finally {
+                client.release();
+            }
         }
-    }
 
     async delete(id: string): Promise<User | undefined> {
+        const client = await pool.connect();
+        
         try {
-            const result = await pool.query("DELETE FROM swe_usrapl su WHERE si.id = $1 RETURNING *", [id]);
+            await client.query('BEGIN');
+
+            // Ejecutar la query de Delete
+            const result = await client.query("DELETE FROM swe_usrapl su WHERE su.id = $1 RETURNING *", [id]);
+            await client.query('COMMIT');
             return result.rows[0];
         } catch (error) {
-            console.error("Error al eliminar usuario")
+            await client.query('ROLLBACK');
+            console.error("Error al eliminar usuario", error);
             throw error;
+        } finally {
+            client.release();
         }
     }
 }
