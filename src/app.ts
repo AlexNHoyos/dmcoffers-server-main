e4dda41bccf74d43eb39a0705e5bd945dcc654b9
Ignacio TR <ignacio_nob_93@hotmail.com>
2024-06-22 14:20:30 -0300

    errorHandler añadido y descomentado en app.ts lo primero de los otros routes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
-/* // Importamos el módulo 'express' para crear un servidor web
+// Importamos el módulo 'express' para crear un servidor web
 // Importamos el enrutador para las rutas relacionadas con los editores
 import express from 'express';
 import userRouter from './routes/usuarios/user.routes.js';
 import { publisherRouter } from './routes/publicadores/publisher.routes.js';
 import { categoriaRouter } from './routes/categorias/categorias.routes.js';
-
+import hostingRoutes from './routes/hosting/hosting.routes.js';
+import errorHandler from './middleware/errorHandler/errorHandler.js';
 // Creamos una instancia de la aplicación Express
 
 const app = express();
@@ -15,6 +16,10 @@ app.use(express.json());
 app.use('/api/users', userRouter);
 app.use('/api/publishers', publisherRouter);
 app.use('/api/categories', categoriaRouter);
+app.use('/api/hostings', hostingRoutes);
+
+
+app.use(errorHandler);
 
 
 // Middleware para manejar solicitudes a rutas no encontradas
@@ -28,28 +33,13 @@ app.use((_, res) => {
 });
 
 // Iniciamos el servidor Express en el puerto 3000
-app.listen(3000, () => {
-  console.log('Servidor corriendo en http://localhost:3000/');
-});
 
-// Para correr esto hay que hacer npm run start:dev en terminal
- */
-
-// src/app.ts
-import express from 'express';
-import userRoutes from './routes/usuarios/user.routes.js';
-import hostingRoutes from './routes/hosting/hosting.routes.js';
-
-const app = express();
-
-app.use(express.json());
-app.use('/api/users', userRoutes);
-app.use('/api/hostings', hostingRoutes);
-
-// Iniciar el servidor
+// Para correr esto hay que hacer npm run start-dev en terminal
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`); // necesita comillas invertidas para que tome el valor de port como variable y no convierta el texto completo en string
 });
 
-export default app;
+
+
+
