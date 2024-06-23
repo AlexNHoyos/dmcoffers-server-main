bc6a78ee95acdda563a5793d024cd8706781c46f
Ignacio TR <ignacio_nob_93@hotmail.com>
2024-06-22 23:09:48 -0300

    modificado error handler para que sea mas adaptable

--- src/middleware/errorHandler/errorHandler.ts
+++ src/middleware/errorHandler/errorHandler.ts
@@ -1,8 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
 
-function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
-    console.error(err.stack);
-    res.status(500).json({ message: 'Ocurrió un error en el servidor', error: err.message });
+interface CustomError extends Error {
+  status?: number;
 }
+
+const errorHandler = (err: CustomError, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack || err);
+
+  const status = err.status || 500;
+  const name = err.name || 'Internal Server Error';
+  const message = err.message || undefined;
+  const stack = err.stack || undefined;
+
+  res.status(status).json({
+    name,
+    status,
+    message,
+    stack,
+  });
+};
 
 export default errorHandler;
