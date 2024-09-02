d71ced034b779db55024c5ea96d86d272b85bec5
Ignacio TR <ignacio_nob_93@hotmail.com>
2024-09-01 23:18:20 -0300

    agregando decoradores a hosting

--- src/models/hosting/hosting.entity.ts
+++ src/models/hosting/hosting.entity.ts
@@ -1,13 +1,47 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('hs_hosting_service') // El nombre de la tabla en la base de datos
+
 export class Hosting {
+
+    @PrimaryGeneratedColumn()
+    public id: string ;
+
+    @Column()
+    public name: string ;
+
+    @Column()
+    public creationuser: string ;
+
+    @Column()
+    public creationtimestamp: Date ;
+
+    @Column({ nullable: true })
+    public modificationuser: string ;
+
+    @Column({ nullable: true })
+    public modificationtimestamp: Date ;
+
+    @Column()
+    public status: boolean ;
+
+
     constructor(
-        public id: string,
-        public name: string,
-        public creationuser: string,
-        public creationtimestamp: Date,
-        public modificationuser: string,
-        public modificationtimestamp: Date,
-        public status: boolean
+        id: string,
+        name: string,
+        creationuser: string,
+        creationtimestamp: Date,
+        modificationuser: string,
+        modificationtimestamp: Date,
+        status: boolean
     ) {
+        this.id = id;
+        this.name = name;
+        this.creationuser = creationuser;
+        this.creationtimestamp = creationtimestamp;
+        this.modificationuser = modificationuser;
+        this.modificationtimestamp = modificationtimestamp;
+        this.status = status;
     }
 }
 
