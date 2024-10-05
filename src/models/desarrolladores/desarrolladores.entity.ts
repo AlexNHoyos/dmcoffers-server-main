0202cc784021854c923d7654d11b5bfc0f0e1684
Mauro Marcosano <mauromarcosano@gmail.com>
2024-10-05 13:52:40 -0300

    Cambio tipo columna de dissolution_date en publisher y desarrolladores porque typeORM filtraba solo las que estaban NULL en los GET

--- src/models/desarrolladores/desarrolladores.entity.ts
+++ src/models/desarrolladores/desarrolladores.entity.ts
@@ -1,56 +1,60 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
 
 @Entity('pub_game_developer') // El nombre de la tabla en la base de datos
-
 export class Desarrollador {
+  @PrimaryGeneratedColumn()
+  public id: number;
 
-    @PrimaryGeneratedColumn()
-    public id: number;
+  @Column()
+  public developername: string;
 
-    @Column()
-    public developername: string;
-    
-    @Column({type: "timestamp"})
-    public foundation_date: Date;
-    
-    @DeleteDateColumn({ nullable: true })
-    public dissolution_date: Date;
-    
-    @Column()
-    public status: boolean;
-    
-    @CreateDateColumn()
-    public creationtimestamp: Date;  
-    
-    @Column()
-    public creationuser: String;
+  @Column({ type: 'timestamp' })
+  public foundation_date: Date;
 
-    @UpdateDateColumn({ nullable: true })
-    public modificationtimestamp: Date;
-    
-    @Column({ nullable: true })
-    public modificationuser: string;
+  @Column({ type: 'timestamp', nullable: true })
+  public dissolution_date: Date;
 
-    constructor(
-        id: number,
-        developername: string,
-        foundation_date: Date,
-        dissolution_date: Date,
-        status: boolean,
-        creationtimestamp: Date,
-        creationuser: String,
-        modificationtimestamp: Date,
-        modificationuser: string
-    ) { 
-        this.id = id;
-        this.developername = developername;
-        this.foundation_date = foundation_date;
-        this.dissolution_date = dissolution_date;
-        this.status = status;
-        this.creationtimestamp = creationtimestamp;
-        this.creationuser = creationuser;
-        this.modificationtimestamp = modificationtimestamp;
-        this.modificationuser = modificationuser;
+  @Column()
+  public status: boolean;
 
-    }
+  @CreateDateColumn()
+  public creationtimestamp: Date;
+
+  @Column()
+  public creationuser: String;
+
+  @UpdateDateColumn({ nullable: true })
+  public modificationtimestamp: Date;
+
+  @Column({ nullable: true })
+  public modificationuser: string;
+
+  constructor(
+    id: number,
+    developername: string,
+    foundation_date: Date,
+    dissolution_date: Date,
+    status: boolean,
+    creationtimestamp: Date,
+    creationuser: String,
+    modificationtimestamp: Date,
+    modificationuser: string
+  ) {
+    this.id = id;
+    this.developername = developername;
+    this.foundation_date = foundation_date;
+    this.dissolution_date = dissolution_date;
+    this.status = status;
+    this.creationtimestamp = creationtimestamp;
+    this.creationuser = creationuser;
+    this.modificationtimestamp = modificationtimestamp;
+    this.modificationuser = modificationuser;
+  }
 }
