09f318cb0a0072858d239da65e6f693b3a59ac41
Ignacio TR <ignacio_nob_93@hotmail.com>
2024-11-10 19:38:00 -0300

    create ticket-dto

--- src/models-dto/support-ticket/ticket-dto.ts
+++ src/models-dto/support-ticket/ticket-dto.ts
@@ -1,19 +1,29 @@
 export class ticketDto{
-    id: number | undefined;
-    status: boolean | undefined;
-    creationuser: string | undefined;
-    creationtimestamp: Date | undefined;
-    modificationuser: string | undefined;
-    modificationtimestamp: Date | undefined;
-    description: string | undefined;
+    id?: number | undefined;
+    status?: boolean | undefined;
+    creationuser?: string | undefined;
+    creationtimestamp?: Date | undefined;
+    modificationuser?: string | undefined;
+    modificationtimestamp?: Date | undefined;
+    description?: string | undefined;
+    username?: string | undefined;
 
 
     constructor(
-        id: number,
-        description: string,
-        email: string,
+        id?: number | undefined,
+        status?: boolean | undefined,
+        creationuser?: string | undefined,
+        creationtimestamp?: Date | undefined,
+        modificationuser?: string | undefined,
+        modificationtimestamp?: Date | undefined,
+        description?: string | undefined,
     ){
-        this.id = id,
-        this.description = description
+        this.id = id;
+        this.description = description;
+        this.status = status ;
+        this.creationuser = creationuser;
+        this.creationtimestamp = creationtimestamp; 
+        this.modificationuser = modificationuser;
+        this.modificationtimestamp = modificationtimestamp;
     }
 }
