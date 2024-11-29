3aa4e83eb634a1a31c701a6b0f1d80181aeb8102
Alex <alexnicolashoyos@gmail.com>
2024-11-29 16:34:56 -0300

    Agregado de varios archivos de test, error en categoria.test.ts

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  globals: {
-    'ts-jest': {
-      isolatedModules: true,
-    },
+  preset: 'ts-jest', // Usa ts-jest para transpilar archivos .ts
+  testEnvironment: 'node', // Usamos un entorno de pruebas adecuado para Node.js
+  transform: {
+    '^.+\\.ts$': 'ts-jest', // Transforma archivos .ts
+    '^.+\\.js$': 'babel-jest', // También se pueden transformar archivos .js si usas ECMAScript modules
   },
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'], // Asegúrate de que se reconozcan las extensiones de los archivos
+  transformIgnorePatterns: ['/node_modules/'], // Para evitar que Jest intente transformar node_modules
 };
--- src/tests/categorias.test.ts
+++ src/tests/categorias.test.ts
@@ -1,11 +1,10 @@
-import { describe, it, test } from 'node:test';
 import request from 'supertest';
 import app from '../app';
 import { container } from '../config/dependency-injection/inversify.config';
 import { CategoriasController } from '../controllers/categorias/categorias.controller';
 
 //mock de categorias 
-jest.mock('../../services/categorias/categorias.service.js');
+jest.mock('../../controllers/categorias/categorias.controller.js');
 
 describe("findAll categorias", ()=> {
 
@@ -16,7 +15,7 @@ describe("findAll categorias", ()=> {
         jest.clearAllMocks();
       });
 
-    it("", async()=>{
+    it("Should return array with categories", async()=>{
        const mockCategorias = [{id: 1, name: 'Categoria 1'}, { id: 2, name: 'Categoria 2' }] 
        categoriasServiceMock.findAll = jest.fn().mockResolvedValue(mockCategorias);
         
@@ -29,6 +28,32 @@ describe("findAll categorias", ()=> {
         categoriasServiceMock.remove = jest.fn().mockResolvedValue(null);
         const response = await request(app).get('/api/categories/999');
         expect(response).toBe(404);
-        expect(response.body.message).toBe('Categoria no encontrada');
+        expect(response.body.message).toBe('Categorias no encontradas');
+    })
+})
+
+describe("findOne categorie", ()=>{
+    const categoriasServiceMock = container.get(CategoriasController);
+
+    beforeEach(()=> {
+        jest.clearAllMocks();
+    })
+
+    it("Should return one category by id", async()=>{
+        const id = 1;
+        const mockCategoria = {id, name:''};
+        categoriasServiceMock.findOne = jest.fn().mockResolvedValue(mockCategoria);
+
+        const response = await request(app).get('/api/categories');
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual(mockCategoria)
+    })
+
+    it(("Should return 404 not found"), async()=>{
+        categoriasServiceMock.remove = jest.fn().mockResolvedValue(null);
+        const response = await request(app).get('/api/categories/999');
+
+        expect(response).toBe(400);
+        expect(response.body.message).toEqual('Categoria no encontrada');
     })
 })
